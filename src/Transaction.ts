a2caae7f36f4a3e8e8b59074a5fa90d2e7ad2267
Trevin Hofmann <trevinhofmann@gmail.com>
2024-03-12 13:05:28 -0700

    feat: use a mutex to exclusively run transaction sending

--- src/Transaction.ts
+++ src/Transaction.ts
@@ -1,26 +1,31 @@
 import { InfuraProvider, Wallet } from 'ethers';
+import { Mutex } from 'async-mutex';
 import constants from './constants';
 import config from './config';
 
 const provider = new InfuraProvider(137)
 
+const transactionMutex = new Mutex();
+
 const send = async (data: string, gasLimit: bigint): Promise<string> => {
-  const wallet = Wallet.fromPhrase(config.mnemonic, provider);
-  const transaction = {
-    from: wallet.address,
-    to: constants.RAILGUN_SMART_CONTRACT_ADDRESS,
-    data,
-    gasLimit,
-    maxFeePerGas: constants.MAX_FEE_PER_GAS,
-    maxPriorityFeePerGas: constants.MAX_PRIORITY_FEE_PER_GAS,
-    type: constants.TRANSACTION_TYPE,
-  };
-  const transactionResponse = await wallet.sendTransaction(transaction);
-  const receipt = await transactionResponse.wait(3);
-  if (receipt?.hash === undefined) {
-    throw new Error('Transaction failed');
-  }
-  return receipt?.hash;
+  return transactionMutex.runExclusive(async () => {
+    const wallet = Wallet.fromPhrase(config.mnemonic, provider);
+    const transaction = {
+      from: wallet.address,
+      to: constants.RAILGUN_SMART_CONTRACT_ADDRESS,
+      data,
+      gasLimit,
+      maxFeePerGas: constants.MAX_FEE_PER_GAS,
+      maxPriorityFeePerGas: constants.MAX_PRIORITY_FEE_PER_GAS,
+      type: constants.TRANSACTION_TYPE,
+    };
+    const transactionResponse = await wallet.sendTransaction(transaction);
+    const receipt = await transactionResponse.wait(3);
+    if (receipt?.hash === undefined) {
+      throw new Error('Transaction failed');
+    }
+    return receipt?.hash;
+  });
 };
 
 export default {
